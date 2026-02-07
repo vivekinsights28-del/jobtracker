b9ddbd549c0bb70cbdc7b773913d986d8ec7b3db
unknown <Anuj132>
2026-02-08 00:38:19 +0530

    fix root url

--- jobtracker/urls.py
+++ jobtracker/urls.py
@@ -19,10 +19,9 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', include('jobs.urls')),
+    path('', include('jobs.urls')),   # ⭐ VERY IMPORTANT
     path('admin/', admin.site.urls),
 
     path('login/', auth_views.LoginView.as_view(template_name='jobs/login.html'), name='login'),
-
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
