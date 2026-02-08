ac532e0f30a871c18023a26b6062e8337d4eaf14
unknown <Anuj132>
2026-02-08 09:59:39 +0530

    debug home route

--- jobtracker/urls.py
+++ jobtracker/urls.py
@@ -14,14 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+
+# urlpatterns = [
+#     path('', include('jobs.urls')),   # ⭐ VERY IMPORTANT
+#     path('admin/', admin.site.urls),
+
+#     path('login/', auth_views.LoginView.as_view(template_name='jobs/login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+# ]
+from django.http import HttpResponse
+from django.urls import path
+
+def home(request):
+    return HttpResponse("Django is working")
 
 urlpatterns = [
-    path('', include('jobs.urls')),   # ⭐ VERY IMPORTANT
-    path('admin/', admin.site.urls),
-
-    path('login/', auth_views.LoginView.as_view(template_name='jobs/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', home),
 ]
