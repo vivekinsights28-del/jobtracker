4947088b1ad3f3082314137487048925cd4eeae0
unknown <Anuj132>
2026-02-08 09:49:00 +0530

    fix jobs root route

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.job_list, name='job_list'),
+    path('', views.job_list, name='job_list'),  # ⭐ VERY IMPORTANT
     path('add/', views.add_job, name='add_job'),
     path('edit/<int:id>/', views.edit_job, name='edit_job'),
     path('delete/<int:id>/', views.delete_job, name='delete_job'),
